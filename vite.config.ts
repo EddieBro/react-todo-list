69811d370cb7dae9295ed021dde1d88980c728d3
Alexey Gerasimov <eddiebrock13@gmail.com>
2026-08-11 09:50:05 +0500

    правки vite config'a

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     react(),
     checker({ typescript: true, eslint: { lintCommand: 'eslint .' } }),
-    tsconfigPaths(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
   css: {
     modules: {
       localsConvention: 'camelCase',
